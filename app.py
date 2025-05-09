8752913eb999022fe718962594884534f920e359
abdken1359 <abdielkengne6@gmail.com>
2025-05-08 23:18:27 -0300

    Learned dymanic URL

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Imports
-from flask import Flask, render_template
+from flask import Flask, Request
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,7 +9,8 @@ print(__name__)
 @app.route("/")
 def home():
     return'<h1 style="color:green">Hello World!</h1>',201
-
-
-if "__main__" in __name__:
+@app.route("/hi/<int:number1>/<int:number2>")
+def hi(number1,number2):
+    return f'{number1} + {number2} = {number1+number2}',200
+if "__main__" == __name__:
     app.run(debug=True, port=1000,host="0.0.0.0")
