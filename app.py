f18fc59e415a653c8839282442ecaaeecbbb26ca
abdken1359 <abdielkengne6@gmail.com>
2025-05-08 21:22:10 -0300

    Rebuilding

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+print(__name__)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home():
+    return'<h1 style="color:green">Hello World!</h1>',201
 
 
 if "__main__" in __name__:
-    app.run(debug=True, port=2000)
+    app.run(debug=True, port=1000,host="0.0.0.0")
