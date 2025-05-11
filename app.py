bbb7211fc5178f65555f4331f0989d4ff2790c9b
abdken1359 <abdielkengne6@gmail.com>
2025-05-10 21:36:29 -0300

    Making sessions and cookies

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 # Imports
-from flask import Flask, request,render_template 
+from flask import Flask, request,render_template ,send_from_directory,session
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
+import os
 
-app = Flask(__name__,template_folder="templates")
-print(__name__)
-
+app = Flask(__name__,template_folder="templates",static_folder="static",static_url_path="/")
+app.config["UPLOAD_FOLDER"]="uploads/"
+app.secret_key="MYKEY"
 @app.route("/",methods=["GET","POST"])
 def home():
     if request.method=="GET":
@@ -13,18 +14,18 @@ def home():
         myResult=3+4
         evenTens=[10,20,30,40,50,60,70,80,90,100]
         return render_template('index.html',myValue=myValue,myResult=myResult,evenTens=evenTens)
-    elif request.method=="POST":
-        username = request.form.get('username')
-        return f"This is a post request {username}",200
-
-@app.route("/other")
-def other():
-    some_text="Just a text sentence"
-    return render_template("other.html",some_text=some_text)
-
-@app.route("/file_upload",methods=["POST"])
-def file_upload():
-     return "Almost done with the magic"
+  
+@app.route("/set_session")
+def set_session():
+    session["name"]="Abdiel"
+    session["age"]=18
+    return render_template("index.html",message="Session data")
+@app.route("/get_session")
+def get_session():
+    name=session["name"]
+    age=session["age"]
+    return render_template("index.html")
+     
 
 @app.template_filter("alternate_case")
 def alternate_case(s):
